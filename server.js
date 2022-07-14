4ecb2381f3c87c0fbf5687181b69419e25d61ac8
heryiswanto <191240000944@unisnu.ac.id>
2022-07-14 15:09:05 +0700

    add backend

---
+++ server.js
@@ -0,0 +1,13 @@
+console.log("Hello husen")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+
+const app = express()
+
+app.use('/api/goals',require('./routes/goalRoutes.js'))
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
+Footer
